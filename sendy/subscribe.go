f3a1f7ca8115bff0be498aa4598777a7952bf3a2
Alma Tuck <alma.tuck@gmail.com>
2023-02-01 02:58:49 -0700

    updated sendy

--- sendy/subscribe.go
+++ sendy/subscribe.go
@@ -7,13 +7,17 @@ import (
 	"github.com/google/go-querystring/query"
 )
 
-func (s *Sendy) Subscribe(in *Subscribe) error {
+func (s *Sendy) Subscribe(in *Subscribe, attribs map[string]interface{}) error {
 	in.ApiKey = &s.Config.ApiKey
 	in.Boolean = true
 
 	postData, _ := query.Values(in)
 	u, _ := url.ParseRequestURI(s.Config.ApiUrl)
 	u.Path = "/subscribe"
+
+	for key, value := range attribs {
+		postData.Set(key, value.(string))
+	}
 
 	body, err := s.do(u.String(), postData)
 
