6a4184593eed0f9a3574d21de2771ce24a9aa080
Alma Tuck <alma.tuck@gmail.com>
2023-01-31 20:51:41 -0700

    added n8n

--- n8n/webhook.go
+++ n8n/webhook.go
@@ -12,6 +12,9 @@ func (s *N8N) Webhook(endpoint string, in map[string]interface{}) (body []byte, 
 		fmt.Println("marshaling data error: ", err)
 	}
 
+	fmt.Println("Webhook endpoint: " + endpoint)
+	fmt.Println("in: ", string(jsonB))
+
 	u, _ := url.ParseRequestURI(s.Config.ApiUrl)
 	u.Path = endpoint
 
